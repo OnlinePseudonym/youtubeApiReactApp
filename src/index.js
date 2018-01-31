9f44303d7e5f50987c9dcbdb2330a3f3b83b3cf7
Kevin Reynolds <Kevin.T.Reynolds30@gmail.com>
2018-01-30 17:42:57 -0700

    install youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyD0F_J3qTuY9TwVKBrSXw0cqSL4thKIC5c';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component. This component should produce some HTML
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return <div>Hi!</div>;
+    // return React.createElement("div", null, "Hi!"); -- alternate syntax
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
